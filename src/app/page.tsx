81cd94cb4bfae95173d303e7ec51812ff03d740b
Brandon Lee <brandonlee2543@hotmail.com>
2024-02-13 20:25:59 -0400

    Removed redirect and updated our min-height

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,9 @@ import Footer from "@/components/Navigation/Footer";
 import Navbar from "@/components/Navigation/Navbar";
 
 export default function Home() {
-  redirect("/construction");
+  // redirect("/construction");
   return (
-    <main className=" flex-col items-center flex bg-slate-400 min-h-[100vh] bg-home-page bg-no-repeat bg-cover gap-4">
+    <main className=" flex-col items-center flex bg-slate-400 min-h-dvh bg-home-page bg-no-repeat bg-cover gap-4">
       <Navbar />
       <Preamble />
       <ButtonContainer />
