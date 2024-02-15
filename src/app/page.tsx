aa7a34bfa16eea3201fb08dc549d09ec431144d8
Brandon Lee <brandonlee2543@hotmail.com>
2024-02-14 21:34:16 -0400

    Added dynamic placeholder ot ouyr background image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { redirect } from "next/navigation";
 
 import ButtonContainer from "@/components/HomePage/ButtonContainer";
@@ -5,11 +7,48 @@ import ChatBox from "@/components/HomePage/ChatBox";
 import Preamble from "@/components/HomePage/Preamble";
 import Footer from "@/components/Navigation/Footer";
 import Navbar from "@/components/Navigation/Navbar";
+import { useEffect, useState } from "react";
+import { placeholderURL } from "./consts/largeText";
 
 export default function Home() {
+  const [backgroundImage, setBackgroundImage] = useState<string | null>(
+    placeholderURL
+  );
+
   // redirect("/construction");
+  useEffect(() => {
+    const preloadImage = async (src: string) => {
+      try {
+        const img = new Image();
+        await new Promise<void>((resolve, reject) => {
+          img.onload = () => {
+            setBackgroundImage(src); // Set the background image once it's loaded
+            resolve();
+          };
+          img.onerror = reject;
+          img.src = src;
+        });
+      } catch (error) {
+        console.error(`Error preloading image: ${src}`, error);
+      }
+    };
+
+    const imageSrc = "/images/kirby_1080_edit_compressed.png";
+
+    console.warn({
+      preloadLog: preloadImage(imageSrc),
+    });
+  }, []); // Empty dependency array ensures this effect runs only once on mount
+
   return (
-    <main className="flex-col items-center flex bg-slate-400 min-h-dvh bg-home-page bg-no-repeat bg-cover gap-4 pb-12 sm:grid sm:grid-rows-5 grid-cols-5">
+    // bg-home-page
+    <main
+      className="flex-col items-center flex bg-slate-400 min-h-dvh
+      bg-no-repeat bg-cover gap-4 pb-12 sm:grid sm:grid-rows-5 grid-cols-5"
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+      }}
+    >
       <Navbar />
       <Preamble />
       <ButtonContainer />
