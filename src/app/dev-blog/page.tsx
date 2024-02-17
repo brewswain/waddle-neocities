28138b9cde7daf8c3dd473d3be82bb8d710681a5
Brandon Lee <brandonlee2543@hotmail.com>
2024-02-17 03:56:55 -0400

    Made new Blogpost

--- src/app/dev-blog/page.tsx
+++ src/app/dev-blog/page.tsx
@@ -3,12 +3,15 @@ import { PostMetaDataProp, getAllPostsMetaData } from "@/app/dev-blog/utils";
 import Navbar from "@/components/Navigation/Navbar";
 
 import PostsContainer from "@/components/DevBlog/PostsContainer";
+import TagSelection from "@/components/Tags/TagSelection";
 
 const DevBlog = async () => {
   const posts: PostMetaDataProp[] = await getAllPostsMetaData();
 
   return (
     <main className="bg-slate-100 min-h-dvh">
+      <Navbar />
+      <TagSelection />
       <PostsContainer posts={posts} />
     </main>
   );
