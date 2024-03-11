fc396b5629afa5e47441dd6a17b29e45cee1c9dc
Brandon Lee <brandonlee2543@hotmail.com>
2024-03-11 09:42:21 -0400

    Add some concepts to my upcoming pages

---
+++ src/app/music/page.tsx
@@ -0,0 +1,24 @@
+
+const MusicPage = () => {
+
+    /*
+    Ok so this one's gonna be crazy. I want to have a playlist builder, but that's the last feature for sure since i want to use rust for that.
+    
+    I need to consume spotify api probably, since i want some artist info etc. I want to get album artwork,  etc for each individual "review" That i'll do.
+
+    However, on the main music page, i want a...personal analyhsis running. (One that shows the type of songs i've been listening too divided by genre, mood, geolocation etc.) 
+
+    If possible it'd also be cool if a playlist gets generated based on my top 20 songs at build time.
+    */
+
+    /* 
+        For the individual posts, i want it to be pretty visually striking with a background and accent colour determined by the album artwork's colour. Some sort of color extraction'll be needed.
+
+        Aldo i want to have reviewCards be pretty as h4ell, probably have the Album artwork be the hero image.
+    */
+  return (
+    <div>MusicPage</div>
+  )
+}
+
+export default MusicPage
---
+++ src/app/oc-corner/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const OCCornerPage = () => {
+
+    /* 
+        Open up with a quote, italic, large text underneat Character's name.
+        Provide some art after commissions get made
+
+        have sections detailed , make this an interesting typography section
+    */
+  return (
+    <div>OCCornerPage</div>
+  )
+}
+
+export default OCCornerPage
