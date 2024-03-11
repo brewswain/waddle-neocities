1121725133625800a449fe203d7dd208d027d4a5
Brandon Lee <brandonlee2543@hotmail.com>
2024-03-11 06:07:01 -0400

    styling shifts

--- src/app/webring/page.tsx
+++ src/app/webring/page.tsx
@@ -2,11 +2,11 @@ import Navbar from "@/components/Navigation/Navbar";
 
 const WebRingPage = () => {
   return (
-    <main className="flex flex-col justify-center pt-10">
+    <main className="flex flex-col items-center pt-10">
         <div className="sticky top-0 z-10 overflow-auto sm:ml-4 sm:flex sm:h-[530px] sm:flex-col sm:self-center md:mt-[20px] lg:h-[60px] lg:w-[830px]">
           <Navbar includeVerticalView />
         </div>
-      <div>hi</div>
+      <div className="text-4xl font-semibold my-8 font-merriweather">hi</div>
     </main>
   );
 };
