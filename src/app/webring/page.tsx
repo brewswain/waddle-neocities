69c74de9d2590d04118897f0c6040e5626a362cf
Brandon Lee <brandonlee2543@hotmail.com>
2024-03-10 20:15:17 -0400

    Create page.tsx

---
+++ src/app/webring/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/Navigation/Navbar";
+
+const WebRingPage = () => {
+  return (
+    <>
+    <Navbar includeVerticalView />
+      <div>hi</div>
+    </>
+  );
+};
+
+export default WebRingPage;
