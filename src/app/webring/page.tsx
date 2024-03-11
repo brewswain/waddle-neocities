35bb5e174c9255f4f45ae9be84c44add54a39dbd
Brandon Lee <brandonlee2543@hotmail.com>
2024-03-11 09:45:42 -0400

    styling changes

--- src/app/webring/page.tsx
+++ src/app/webring/page.tsx
@@ -4,10 +4,10 @@ import Navbar from "@/components/Navigation/Navbar";
 
 const WebRingPage = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center bg-cyan-200 pt-10">
-      <article className="sticky top-0 z-10 overflow-auto sm:ml-4 sm:flex sm:h-[530px] sm:flex-col sm:self-center md:mt-[20px] lg:h-[60px] lg:w-[830px]">
+    <main className="flex min-h-screen flex-col items-center bg-cyan-200 md:pt-10">
+      <div className="sticky top-0 z-10 w-screen overflow-auto sm:ml-4 sm:flex sm:h-[530px] sm:flex-col sm:self-center md:mt-[20px] lg:h-[60px] lg:w-[830px]">
         <Navbar includeVerticalView />
-      </article>
+      </div>
       <h1 className="my-8 mt-10 font-merriweather text-4xl font-semibold">
         Waddle Squad
       </h1>
