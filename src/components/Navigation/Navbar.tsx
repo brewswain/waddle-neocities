46847bccecaf8ba0f93feccc1a2a7bccf2656b8c
Brandon Lee <brandonlee2543@hotmail.com>
2024-03-11 06:06:52 -0400

    Update Navbar.tsx

--- src/components/Navigation/Navbar.tsx
+++ src/components/Navigation/Navbar.tsx
@@ -22,8 +22,7 @@ const Navbar = ({ includeVerticalView }: NavbarProps) => {
     // TODO: Add active Links
 
     <nav
-
-      className={`flex flex-col border border-pink-200 bg-slate-100 px-4 py-2 pl-10  sm:px-10 lg:w-full lg:max-w-full ${includeVerticalView ?  "sm:flex sm:h-full sm:max-w-[200px] sm:self-center sm:justify-self-end md:static md:mr-4 md:max-w-[200px] md:self-end" : "sm:w-[100vw] sm:max-w-[100vw] lg:justify-center lg:gap-[50px]"} navbar__container`}
+      className={`flex flex-col bg-slate-100 px-4 py-2 pl-10  sm:px-10 lg:w-full lg:max-w-full ${includeVerticalView ? "sm:flex sm:h-full sm:max-w-[200px] sm:self-center sm:justify-self-end md:static md:mr-4 md:max-w-[200px] md:self-end" : "sm:w-[100vw] sm:max-w-[100vw] lg:justify-center lg:gap-[50px]"} navbar__container`}
     >
       <button
         type="button"
