cee2a7a47845d6bca09549b932c579ffe78768dc
Brandon Lee <brandonlee2543@hotmail.com>
2024-03-11 09:28:42 -0400

    Added some basic styling to our webring

--- src/app/webring/page.tsx
+++ src/app/webring/page.tsx
@@ -1,12 +1,26 @@
+import FriendBadgeContainer from "@/components/FriendBadges/FriendBadgeContainer";
+import MyFriendBadge from "@/components/FriendBadges/MyFriendBadge";
 import Navbar from "@/components/Navigation/Navbar";
 
 const WebRingPage = () => {
   return (
-    <main className="flex flex-col items-center pt-10">
-      <div className="sticky top-0 z-10 overflow-auto sm:ml-4 sm:flex sm:h-[530px] sm:flex-col sm:self-center md:mt-[20px] lg:h-[60px] lg:w-[830px]">
+    <main className="flex min-h-screen flex-col items-center bg-cyan-200 pt-10">
+      <article className="sticky top-0 z-10 overflow-auto sm:ml-4 sm:flex sm:h-[530px] sm:flex-col sm:self-center md:mt-[20px] lg:h-[60px] lg:w-[830px]">
         <Navbar includeVerticalView />
-      </div>
-      <div className="my-8 font-merriweather text-4xl font-semibold">hi</div>
+      </article>
+      <h1 className="my-8 mt-10 font-merriweather text-4xl font-semibold">
+        Waddle Squad
+      </h1>
+      <article className="flex flex-wrap justify-center rounded bg-white lg:min-w-[600px]">
+        <FriendBadgeContainer />
+      </article>
+
+      <h2 className=" mb-6 mt-12 font-merriweather text-3xl font-semibold">
+        My Button!
+      </h2>
+      <article className="flex flex-wrap justify-center rounded bg-white">
+        <MyFriendBadge />
+      </article>
     </main>
   );
 };
