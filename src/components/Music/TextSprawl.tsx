ab62a4fba2db803823aec23ded6d50531e17b07d
Brandon Lee <brandonlee2543@hotmail.com>
2024-04-02 22:55:55 -0400

    Create TextSprawl.tsx

---
+++ src/components/Music/TextSprawl.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import React from "react";
+
+const TextSprawl = () => {
+  return (
+    <p className="px-2 text-center text-lg font-medium text-white opacity-70">
+      Why don&apos;t you put down your knapsack, grab a drink, and possibly read
+      about some music that&apos;s{" "}
+      <span className=" cursor-pointer border-b border-b-violet-500 italic text-violet-500">
+        <Link href={"/music/hodgepodge"}>currently interesting</Link>
+      </span>{" "}
+      to me at the moment.
+    </p>
+  );
+};
+
+export default TextSprawl;
