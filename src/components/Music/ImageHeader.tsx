db4c465d47f9080eebd4a676d7230370344b3fb1
Brandon Lee <brandonlee2543@hotmail.com>
2024-04-02 22:55:38 -0400

    Update ImageHeader.tsx

--- src/components/Music/ImageHeader.tsx
+++ src/components/Music/ImageHeader.tsx
@@ -6,17 +6,18 @@ interface ImageHeaderProps {
 
 const ImageHeader = ({ playlistData }: ImageHeaderProps) => {
   return (
-    <>
+    <div className="flex flex-col pb-4 md:flex-row">
       <Image
         alt="Album artwork"
         src={playlistData.images[0].url}
         width={450}
         height={450}
+        className="hidden md:block"
       />
-      <h1 className="pl-4 text-9xl font-semibold text-white opacity-80">
-        {playlistData.name}
+      <h1 className=" pt-4 text-center text-4xl font-semibold text-white opacity-80 sm:pl-4 sm:text-left md:text-9xl">
+        Welcome, traveller.
       </h1>
-    </>
+    </div>
   );
 };
 
