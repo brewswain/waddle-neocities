4fabce7a84741996708ac00b8ddca266d7697635
Brandon Lee <brandonlee2543@hotmail.com>
2024-04-02 21:21:27 -0400

    Cleaned up code--might possibly delete

--- src/components/Music/Auth.tsx
+++ src/components/Music/Auth.tsx
@@ -1,9 +1,16 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { usePathname, useSearchParams } from "next/navigation";
 
-import { authorizeUser, getCurrentUser, getUserToken } from "@/app/music/api";
+import { usePathname, useSearchParams } from "next/navigation";
+import { signIn, signOut, useSession } from "next-auth/react";
+
+import {
+  authorizeUser,
+  findUser,
+  getCurrentUser,
+  getUserToken,
+} from "@/app/music/api";
 
 const Auth = () => {
   const [currentUser, setCurrentUser] = useState();
@@ -11,26 +18,7 @@ const Auth = () => {
   const searchParams = useSearchParams();
   const code = searchParams.get("code");
   const state = searchParams.get("state");
-
-  useEffect(() => {
-    if (code && state) {
-      console.log({ code, state });
-      const response = getUserToken(code, state);
-
-      response.then(
-        (user: {
-          access_token: string;
-          token_type: string;
-          expires_in: number;
-          refresh_token: string;
-          scope: string;
-        }) =>
-          getCurrentUser(user.access_token).then((currentUser) =>
-            console.log(currentUser),
-          ),
-      );
-    }
-  }, []);
+  const session = useSession();
 
   return code && state ? null : (
     <button onClick={() => authorizeUser()}>login</button>
